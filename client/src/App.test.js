382a78acbaca20b3e81a65c71e390a08dbddde98
Oleksandr Lytovchenko <allitovchenko91@gmail.com>
2020-11-19 02:45:16 +0200

    small test update

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import React from "react"
+import {render} from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const {container} = render(<App />);
+  const {container} = render(<App/>);
 
   expect(container.firstChild).toHaveClass("App");
 });
