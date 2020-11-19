1d4bf5b3eb89c1aabd8980d70262fcdc30915152
Oleksandr Lytovchenko <allitovchenko91@gmail.com>
2020-11-19 15:48:37 +0200

    fixes with scss, test, and rendering routes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,14 @@
-import React from "react"
-import {render} from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 test("app render", () => {
-  const {container} = render(<App/>);
+  const { container } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
   expect(container.firstChild).toHaveClass("App");
 });
