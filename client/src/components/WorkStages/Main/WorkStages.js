3bb62faba7bd0caf7e821d4f12a76d9d4d21fe66
Oleksandr Lytovchenko <olyt54@gmail.com>
2020-11-26 01:23:53 +0200

    a little bit styles added

--- client/src/components/WorkStages/Main/WorkStages.js
+++ client/src/components/WorkStages/Main/WorkStages.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./WorkStages.scss";
 import WorkStagesList from "../WorkStagesList/WorkStagesList";
 import Button from "../../generalComponents/Button/Button";
 
@@ -6,11 +7,12 @@ const WorkStages = () => {
   return (
     <section className="work-stages">
       <div className="work-stages__content">
+        <h3 className="work-stages__head">Section head</h3>
         <div className="work-stages__items-wrapper">
           <WorkStagesList />
         </div>
         <p className="work-stages__description">description</p>
-        <Button text="ololo" />
+        <Button text="ololo" className="button-callBack-bigger" />
       </div>
     </section>
   );
