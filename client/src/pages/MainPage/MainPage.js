2fcb01247d155474703bb53d305c1c97409ea910
Oleksandr Lytovchenko <olyt54@gmail.com>
2020-11-27 22:17:40 +0200

    main page set to default

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,10 +1,10 @@
 import React from "react";
-import WorkStages from "../../components/WorkStages/Main/WorkStages";
+
 
 const MainPage = () => {
   return (
     <div>
-      <WorkStages />
+
     </div>
   );
 };
