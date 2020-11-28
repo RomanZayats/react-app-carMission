9510ef2a5df844a0b437fb734e625912f9f7ba64
Roman Zayats <zayats.roma@gmail.com>
2020-11-28 21:33:01 +0200

    updated mainPage

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -1,6 +1,4 @@
 import React from "react";
-import AutoFromUsa from "../../sections/AutoFromUSA/AutoFromUSA";
-import AboutUs from "../../sections/AboutUs/AboutUs";
 
 const MainPage = () => {
   return <div></div>;
