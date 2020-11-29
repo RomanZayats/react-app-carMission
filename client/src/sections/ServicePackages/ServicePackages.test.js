442f142e586b52143ee34a673c122549c7c12c12
Kopynets Artem <bachik85@gmail.com>
2020-11-29 22:44:40 +0200

    add test for section ServicePackages and minor fix

---
+++ client/src/sections/ServicePackages/ServicePackages.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ServicePackages from "./ServicePackages";
+import SectionHeading from "../../components/generalComponents/SectionHeading/SectionHeading";
+
+test("ServicePackages is rendered correctly", () => {
+  const mockSectionClassName = "servicePackages";
+  render(<ServicePackages className={mockSectionClassName} />);
+});
+
+test("ServicePackages contains sections", () => {
+  const mockHeadingText = "Test heading";
+  const mockFeatureClassName = "servicePackages";
+  const { getByTestId } = render(
+    <ServicePackages>
+      <SectionHeading text={mockHeadingText} />
+      <div className={mockFeatureClassName} />
+    </ServicePackages>
+  );
+  const headingText = getByTestId("section-heading");
+  expect(headingText).toBeDefined();
+});
--- client/src/sections/ServicePackages/components/ServicePackage.js
+++ client/src/sections/ServicePackages/components/ServicePackage.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "./ServicePackage.scss";
 import { v4 as uuidv4 } from "uuid";
 
-const ServicePackage = ({ className, name, price, currency, serviceList }) => {
+const ServicePackage = ({ name, price, currency, serviceList }) => {
   const servicesLiArr = serviceList.map((i) => <li key={uuidv4()}>{i}</li>);
 
   return (
-    <div className={className}>
-      <div className="servicePackages__item-title">
-        <h3 className="servicePackages__name">{name}</h3>
-        <span className="servicePackages__currency">{currency}</span>
-        <span className="servicePackages__price">{price}</span>
+    <div className="servicePackage">
+      <div className="servicePackage__item-title">
+        <h3 className="servicePackage__name">{name}</h3>
+        <span className="servicePackage__currency">{currency}</span>
+        <span className="servicePackage__price">{price}</span>
       </div>
-      <ul className="servicePackages__serviceList">{servicesLiArr}</ul>
+      <ul className="servicePackage__serviceList">{servicesLiArr}</ul>
     </div>
   );
 };
