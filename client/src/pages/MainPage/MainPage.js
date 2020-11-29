c3872cbb3fb5ed6fb2a5851e6b3a4699cd13d715
Oleksandr Lytovchenko <olyt54@gmail.com>
2020-11-29 09:33:55 +0200

    working with memo

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -12,9 +12,12 @@ const MainPage = () => {
   const mapComponentsToRender = () => {
     return sectionsFromDB.map((section) => {
       const { description, _id: id, heading, name, reactComponent } = section;
-      const Component = sectionsComponents.find(
-        (component) => component.name === reactComponent
-      );
+      const Component = sectionsComponents.find((component) => {
+        if (component.type) {
+          return component.type.name === reactComponent;
+        }
+        return component.name === reactComponent;
+      });
 
       if (Component) {
         return (
