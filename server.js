47518a2fb6c2325802700f0823d3dee7427b8c5a
olha-s <69920194+olha-s@users.noreply.github.com>
2020-12-03 00:27:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+require("dotenv').config();
 
 const globalConfigs = require("./routes/globalConfigs");
 const customers = require("./routes/customers");
