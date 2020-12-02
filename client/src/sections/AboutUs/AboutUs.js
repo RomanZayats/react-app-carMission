fb2e866d99ddc5b85b5169f94c8d9e0a80f549ef
Roman Zayats <zayats.roma@gmail.com>
2020-12-02 15:46:08 +0200

    fixed due to comments

--- client/src/sections/AboutUs/AboutUs.js
+++ client/src/sections/AboutUs/AboutUs.js
@@ -1,69 +1,67 @@
-import React, {memo, useEffect, useState} from "react";
+import React, { memo, useEffect, useState } from "react";
 import "./AboutUs.scss";
 import axios from "axios";
 import RegularFeature from "./components/RegularFeature/RegularFeature";
 import MainFeature from "./components/MainFeature/MainFeature";
 import SectionHeading from "../../components/generalComponents/SectionHeading/SectionHeading";
 
-const AboutUs = ({heading, anchorName}) => {
-    const [featuresList, setFeaturesList] = useState([]);
+const AboutUs = ({ heading, anchorName }) => {
+  const [featuresList, setFeaturesList] = useState([]);
 
-    useEffect(() => {
-        getFeatures();
-    }, []);
+  useEffect(() => {
+    getFeatures();
+  }, []);
 
-    const getFeatures = async () => {
-        const featuresFromServer = await axios("/api/features/").then(
-            (res) => res.data
-        );
-        setFeaturesList(featuresFromServer);
-    };
+  const getFeatures = async () => {
+    const featuresFromServer = await axios("/api/features/").then(
+      (res) => res.data
+    );
+    setFeaturesList(featuresFromServer);
+  };
 
-    const featuresFilter = () => {
-        const regularFeaturesArr = featuresList.filter(f => !f.isMain);
-        const mainFeatureArr = featuresList.filter(f => f.isMain);
+  const featuresRender = () => {
+    const regularFeaturesArr = featuresList.filter((f) => !f.isMain);
+    const mainFeatureArr = featuresList.filter((f) => f.isMain);
 
-        const regularFeaturesToRender = regularFeaturesArr.map((regularFeature) => {
-            const {imgPath, title, _id: id} = regularFeature;
-            return (
-                <RegularFeature
-                    className="about-us__feature-box"
-                    imgPath={imgPath}
-                    title={title}
-                    key={id}
-                />
-            );
-        })
+    const regularFeaturesToRender = regularFeaturesArr.map((regularFeature) => {
+      const { imgPath, title, _id: id } = regularFeature;
+      return (
+        <RegularFeature
+          className="about-us__feature-box"
+          imgPath={imgPath}
+          title={title}
+          key={id}
+        />
+      );
+    });
 
-        const mainFeatureToRender = mainFeatureArr.map((mainFeature) => {
-            const {imgPath, title, text, _id: id} = mainFeature;
-            return (
-                <MainFeature
-                    className="about-us__main-feature-box"
-                    imgPath={imgPath}
-                    title={title}
-                    text={text}
-                    key={id}
-                />
-            );
-        });
-
-        return (
-            <>
-                <div className="about-us__features-box">
-                    {regularFeaturesToRender}
-                </div>
-                {mainFeatureToRender}
-            </>
-        )
-    }
+    const mainFeatureToRender = mainFeatureArr.map((mainFeature) => {
+      const { imgPath, title, text, _id: id } = mainFeature;
+      return (
+        <MainFeature
+          className="about-us__main-feature-box"
+          imgPath={imgPath}
+          title={title}
+          text={text}
+          key={id}
+        />
+      );
+    });
 
     return (
-        <section className="about-us__container" id={anchorName}>
-            <SectionHeading className="about-us__heading" text={heading}/>
-            {featuresFilter(featuresList)}
-        </section>
+      <>
+        <div className="about-us__features-box">{regularFeaturesToRender}</div>
+        {mainFeatureToRender}
+      </>
     );
+  };
+
+  return (
+    <section className="about-us__container" id={anchorName}>
+      <SectionHeading className="about-us__heading" text={heading} />
+      {featuresRender(featuresList)}
+    </section>
+  );
 };
 
 export default memo(AboutUs);
--- client/src/sections/AboutUs/components/MainFeature/components/MainFeatureImage/MainFeatureImage.js
+++ client/src/sections/AboutUs/components/MainFeature/components/MainFeatureImage/MainFeatureImage.js
@@ -1,17 +1,16 @@
 import React from "react";
-import "./MainFeatureImage.scss";
 import Image from "../../../../../../components/Image/Image";
 
-const MainFeatureImage = ({imgPath}) => {
-    return (
-        <div className="about-us__main-feature-image-box">
-            <Image
-                className="about-us__main-feature-image"
-                src={imgPath}
-                alt="staff-photo"
-            />
-        </div>
-    );
+const MainFeatureImage = ({ imgPath }) => {
+  return (
+    <div className="about-us__main-feature-image-box">
+      <Image
+        className="about-us__main-feature-image"
+        src={imgPath}
+        alt="staff-photo"
+      />
+    </div>
+  );
 };
 
 export default MainFeatureImage;
