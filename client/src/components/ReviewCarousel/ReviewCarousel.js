80a2d1d507aa85954186bda30c999fd0b6952801
olha-s <69920194+olha-s@users.noreply.github.com>
2020-12-03 14:42:19 +0200

    Update ReviewCarousel.js

--- client/src/components/ReviewCarousel/ReviewCarousel.js
+++ client/src/components/ReviewCarousel/ReviewCarousel.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ReviewItem from "./ReviewItem/ReviewItem";
 import SectionHeading from "../generalComponents/SectionHeading/SectionHeading";
