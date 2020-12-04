bafe1cd86597a2d1354b4be6b9e3c8d0c5731002
Oleksandr Lytovchenko <olyt54@gmail.com>
2020-12-04 10:12:39 +0200

    key refactor

--- client/src/components/PaginationDots/PaginationDots.js
+++ client/src/components/PaginationDots/PaginationDots.js
@@ -3,13 +3,9 @@ import PaginationDotItem from "../PaginationDotItem/PaginationDotItem";
 import "./PaginationDots.scss";
 
 const PaginationDots = ({ componentsList }) => {
-  const dotsList = componentsList.map((section, index) => {
+  const dotsList = componentsList.map((section) => {
     return (
-      <PaginationDotItem
-        key={index}
-        anchor={section.props.anchorName}
-        // isVisible={section.props.children.props.isVisible}
-      />
+      <PaginationDotItem key={Date.now} anchor={section.props.anchorName} />
     );
   });
 
