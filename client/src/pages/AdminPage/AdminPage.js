7b8b1da0d0ae48e602f90cd8734cfc6c1eaffcd5
Олександр Заяць <ovzayatz@gmail.com>
2020-12-09 20:15:19 +0200

    main page sections on admin page added

--- client/src/pages/AdminPage/AdminPage.js
+++ client/src/pages/AdminPage/AdminPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import AdminHeader from "../../components/admin/AdminHeader/AdminHeader";
 import SideBar from "../../components/admin/SideBar/SideBar";
 import FormContainerWorkStages from "../../components/admin/WorkStages/FormContainer/FormContainerWorkStages";
+import FormContainerMainPageSections from "../../components/admin/MainPageSections/FormContainer/FormContainer";
 
 const AdminPage = () => {
   return (
     <>
       <AdminHeader />
       <SideBar />
+      <FormContainerMainPageSections/>
       <FormContainerWorkStages />
     </>
   );
