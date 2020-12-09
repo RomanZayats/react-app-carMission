82c788fdcfb9f91cbef9327465b58f0a67aa2bb8
Oleksandr Lytovchenko <olyt54@gmail.com>
2020-12-09 18:12:18 +0200

    greatest shit have been happened

--- client/src/pages/AdminPage/AdminPage.js
+++ client/src/pages/AdminPage/AdminPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AdminHeader from "../../components/admin/AdminHeader/AdminHeader";
 import SideBar from "../../components/admin/SideBar/SideBar";
+import FormContainerWorkStages from "../../components/admin/WorkStages/FormContainer/FormContainerWorkStages";
 
 const AdminPage = () => {
   return (
     <>
       <AdminHeader />
-      <SideBar/>
+      <SideBar />
+      <FormContainerWorkStages />
     </>
   );
 };
