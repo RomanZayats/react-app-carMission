6acca9b80b2cbee5ed17d4d209f2165b81570d96
Konstantin Front <frontkot@gmail.com>
2020-12-11 18:45:11 +0200

    add delete and put methods, delete unused code

--- routes/navbar.js
+++ routes/navbar.js
@@ -6,21 +6,42 @@ const passport = require("passport");
 const {
     addNavbarItem,
     getNavbarItem,
-    deleteNavbarItem
+    getNavbarItems,
+    deleteNavbarItem,
+    updateNavbarItem
 } = require("../controllers/navbar");
 
-// @route   POST /catalog
-// @desc    Create new category
-// @access  Private
-router.post("/", addNavbarItem);
-
-// @route   GET /catalog
-// @desc    GET existing categories
+// @route   GET /navbar
+// @desc    GET existing navbar items
 // @access  Public
-router.get("/", getNavbarItem);
+router.get("/", getNavbarItems);
 
-// @route   DELETE /links/:id
-// @desc    DELETE existing links
+// @route   GET /navbar
+// @desc    GET existing  navbar item
+// @access  Public
+router.get("/:id", getNavbarItem);
+
+
+// @route   POST /navbar
+// @desc    Create new navbar item
+// @access  Private
+router.post(
+    "/", 
+    // passport.authenticate("jwt-admin", { session: false }),
+    addNavbarItem
+);
+
+// @route   PUT /navbar/:id
+// @desc    Update existing navbar item
+// @access  Private
+router.put(
+    "/:id",
+    // passport.authenticate("jwt-admin", { session: false }),
+    updateNavbarItem
+);
+
+// @route   DELETE /navbar/:id
+// @desc    DELETE existing navbar item
 // @access  Private
 router.delete(
     "/:id",
