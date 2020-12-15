e43a54b0700f832b1a270223c1f19f9bf6249b4a
Konstantin Front <frontkot@gmail.com>
2020-12-15 20:49:49 +0200

    some edits in admin navbar components

--- client/src/components/admin/AdminNavbar/AdminNavbar.js
+++ client/src/components/admin/AdminNavbar/AdminNavbar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getNavbarData } from "../../../../store/navbar/selectors";
+import AdminNavarItem from "../AdminNavbarItem/AdminNavarItem";
 
 
 const AdminNavbar = () => {
     const navbarData = useSelector(getNavbarData);
-    
 
+    const navbarItems = navbarData.map(({ textContent, contacts, footerLocation, headerLocation, disabled, _id }, index) => 
+        <AdminNavarItem 
+            className="admin__navbar-item"
+            textContent={textContent}
+            disabled={disabled}
+            headerLocation={headerLocation}
+            footerLocation={footerLocation}
+            contacts={contacts}
+            key={index}
+        />
+    )
+    
     return (
-        <div>
-            
-        </div>
+        <ul>{navbarItems}</ul>
     );
 };
 
--- client/src/components/admin/AdminNavbarItem/AdminNavarItem.js
+++ client/src/components/admin/AdminNavbarItem/AdminNavarItem.js
@@ -1,12 +1,75 @@
 import React from 'react';
+import { Formik } from 'formik';
+import AdminFormField from '../AdminFormField/AdminFormField';
 
 const AdminNavarItem = ({
-    textContent, sectionId, headerLocation, footerLocation, disabled
+    className, textContent, headerLocation, footerLocation, disabled, _id
 }) => {
     return (
-        <div>
-            
-        </div>
+        <Formik
+        // initialValues={{ imgPath, title }}
+        // validationSchema={validationSchema}
+        validateOnChange={false}
+        validateOnBlur={false}
+        onSubmit={onSubmit}
+        > 
+            {({ errors, touched }) => (
+                <Form className={className}>
+                    <AdminFormField
+                        className="admin__form-label"
+                        type="input"
+                        name="section-name"
+                        errors={errors}
+                        labelName="Название секции в навбаре"
+                    />
+
+                    <AdminFormField
+                        as="select"
+                        className="admin__form-label"
+                        type="input"
+                        name="headerLocation"
+                        errors={errors}
+                        labelName="Положение в меню"
+                    >
+                        <option value="left-side">Слева от лого</option>
+                        <option value="right-side">Справа от лого</option>
+                        <option value="non-active">Неактивно в меню</option>
+                    </AdminFormField>
+
+                    <AdminFormField
+                        as="select"
+                        className="admin__form-label"
+                        type="input"
+                        name="footerLocation"
+                        errors={errors}
+                        labelName="Положение в футере"
+                    >
+                        <option value="left-side">Слева от лого</option>
+                        <option value="right-side">Справа от лого</option>
+                        <option value="non-active">Неактивно в футере</option>
+                    </AdminFormField>
+
+
+                    <div className="admin__buttons-box">
+                        <Button
+                            className="admin__delete-btn"
+                            text="Delete item"
+                            onClick={(event) => {
+                                event.preventDefault();
+                            }}
+                        />
+                        <Field
+                            type="submit"
+                            disabled={isUpdated}
+                            name="submit"
+                            className="admin__submit-btn"
+                            value="Submit changes"
+                        />
+                        {isUpdated && <UpdateConfirmation />}
+                    </div>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
