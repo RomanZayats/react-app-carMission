ce595a903c5200f96692e832cd59c370fb3f5732
Oleksandr Lytovchenko <olyt54@gmail.com>
2021-02-06 19:20:34 +0200

    account btn

--- client/src/components/admin/AdminHeader/AdminHeader.js
+++ client/src/components/admin/AdminHeader/AdminHeader.js
@@ -4,6 +4,7 @@ import "./AdminHeader.scss";
 import { useDispatch } from "react-redux";
 import { setIsAuth } from "../../../store/auth/actions";
 import axios from "axios";
+import AccountIcon from "./SVG/AccountIcon";
 
 const AdminHeader = () => {
   const dispatch = useDispatch();
@@ -17,11 +18,24 @@ const AdminHeader = () => {
   return (
     <div className="admin-header">
       <h3 className="admin-header__head">Admin Page</h3>
-      <Button
-        className="button2-send-request"
-        text="Logout"
-        onClick={handleLogout}
-      />
+      <div className="admin-header__btn-wrapper">
+        <Button text={<AccountIcon />} className="admin-header__account" />
+        {/* <Button */}
+        {/*  className="button2-send-request" */}
+        {/*  text="Change login" */}
+        {/*  onClick={handleLogout} */}
+        {/* /> */}
+        {/* <Button */}
+        {/*  className="button2-send-request" */}
+        {/*  text="Change password" */}
+        {/*  onClick={handleLogout} */}
+        {/* /> */}
+        <Button
+          className="button2-send-request"
+          text="Logout"
+          onClick={handleLogout}
+        />
+      </div>
     </div>
   );
 };
