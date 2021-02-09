9f0888ef10ac014828da799888651e31d326f0d3
Oleksandr Lytovchenko <olyt54@gmail.com>
2021-02-09 14:03:20 +0200

    remove post and delete from admin main sections

--- client/src/components/admin/MainPageSections/FormItem/FormItem.js
+++ client/src/components/admin/MainPageSections/FormItem/FormItem.js
@@ -4,76 +4,31 @@ import "./FormItem.scss";
 import AdminFormField from "../../AdminFormField/AdminFormField";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import Button from "../../../generalComponents/Button/Button";
 import { validationSchema } from "../validationSchema";
 import { loadMainSection } from "../../../../store/appMainSections/operations";
 import { toastr } from "react-redux-toastr";
 
-const FormItem = ({ obj, sectionCreationStatus, setSectionCreationStatus }) => {
-  const {
-    heading,
-    description,
-    index,
-    disabled,
-    name,
-    reactComponent,
-    _id,
-  } = obj;
+const FormItem = ({ obj }) => {
+  const { heading, description, index, disabled, name, _id } = obj;
   const dispatch = useDispatch();
 
   const onSubmit = async (values) => {
-    if (sectionCreationStatus === "creating") {
-      const sectionToServer = await axios({
-        method: "POST",
-        url: "/api/sections-main/",
-        data: { ...values, reactComponent: "newSectionComponent" },
-      }).catch((err) => {
-        toastr.error(err.message);
-      });
+    const updatedObj = { ...obj, ...values };
 
-      if (sectionToServer.status === 200) {
-        setSectionCreationStatus("created");
-        dispatch(loadMainSection());
-        toastr.success(
-          "Успешно",
-          `Секция "${values.name}" создана в базе данных`
-        );
-      } else {
-        toastr.warning("Хм...", "Что-то пошло не так");
-      }
-    } else {
-      const updatedObj = { ...obj, ...values };
-
-      const sectionToServer = await axios({
-        method: "PUT",
-        url: `/api/sections-main/${_id}`,
-        data: updatedObj,
-      }).catch((err) => {
-        toastr.error(err.message);
-      });
-
-      if (sectionToServer.status === 200) {
-        dispatch(loadMainSection());
-        toastr.success(
-          "Успешно",
-          `Секция "${values.name}" изменена в базе данных`
-        );
-      } else {
-        toastr.warning("Хм...", "Что-то пошло не так");
-      }
-    }
-  };
-
-  const deleteSection = async () => {
-    const delSectionFromServer = await axios({
-      method: "DELETE",
+    const sectionToServer = await axios({
+      method: "PUT",
       url: `/api/sections-main/${_id}`,
+      data: updatedObj,
     }).catch((err) => {
       toastr.error(err.message);
     });
-    if (delSectionFromServer.status === 200) {
+
+    if (sectionToServer.status === 200) {
       dispatch(loadMainSection());
-      toastr.success("Успешно", `Секция "${name}" удалена в базе данных`);
+      toastr.success(
+        "Успешно",
+        `Секция "${values.name}" изменена в базе данных`
+      );
     } else {
       toastr.warning("Хм...", "Что-то пошло не так");
     }
@@ -87,27 +42,24 @@ const FormItem = ({ obj, sectionCreationStatus, setSectionCreationStatus }) => {
         index,
         disabled,
         name,
-        reactComponent,
       }}
       validationSchema={validationSchema}
       validateOnChange={false}
       validateOnBlur={false}
-      onSubmit={(values) => {
-        onSubmit(values);
-      }}
+      onSubmit={onSubmit}
     >
-      {({ errors, touched }) => (
+      {({ errors }) => (
         <Form className="admin__form-item">
           <AdminFormField
             className="admin__form-label"
-            type="input"
+            type="text"
             name="heading"
             errors={errors}
             labelName="Заголовок"
           />
           <AdminFormField
             className="admin__form-label"
-            type="input"
+            type="text"
             name="description"
             errors={errors}
             labelName="Описание"
@@ -115,14 +67,14 @@ const FormItem = ({ obj, sectionCreationStatus, setSectionCreationStatus }) => {
           />
           <AdminFormField
             className="admin__form-label"
-            type="input"
+            type="text"
             name="index"
             errors={errors}
             labelName="Порядок при отображении"
           />
           <AdminFormField
             className="admin__form-label"
-            type="input"
+            type="text"
             name="name"
             errors={errors}
             labelName="Название секции"
@@ -139,46 +91,15 @@ const FormItem = ({ obj, sectionCreationStatus, setSectionCreationStatus }) => {
             </span>
           </label>
 
-          {sectionCreationStatus === "creating" ||
-          sectionCreationStatus === "created" ? (
-            <div className="admin__buttons-box">
-              <Button
-                className="admin__delete-btn"
-                text="&#10005;"
-                onClick={(event) => {
-                  event.preventDefault();
-                  setSectionCreationStatus("no");
-                }}
-              />
-              <Field
-                type="submit"
-                name="submit"
-                className="admin__submit-btn"
-                value="Submit"
-              />
-            </div>
-          ) : (
-            <div className="admin__buttons-box">
-              <Button
-                className="admin__delete-btn"
-                text="&#10005;"
-                onClick={(event) => {
-                  event.preventDefault();
-                  deleteSection();
-                }}
-              />
-              <Field
-                type="submit"
-                name="submit"
-                className="admin__submit-btn"
-                value="Submit changes"
-              />
-            </div>
-          )}
+          <Field
+            type="submit"
+            name="submit"
+            className="admin__submit-btn"
+            value="Submit changes"
+          />
         </Form>
       )}
     </Formik>
   );
 };
-
 export default FormItem;
