d1728ef9e06e062f189af5f6d910ca5705b77d53
Oleksandr Lytovchenko <olyt54@gmail.com>
2021-02-11 23:12:33 +0200

    uuid unused

--- client/src/components/PaginationDots/PaginationDots.js
+++ client/src/components/PaginationDots/PaginationDots.js
@@ -1,11 +1,10 @@
 import React from "react";
 import PaginationDotItem from "../PaginationDotItem/PaginationDotItem";
 import "./PaginationDots.scss";
-import { v4 as uuidv4 } from "uuid";
 
 const PaginationDots = ({ componentsList }) => {
   const dotsList = componentsList.map((section) => {
-    return <PaginationDotItem key={uuidv4()} anchor={section.name} />;
+    return <PaginationDotItem key={section._id} anchor={section.name} />;
   });
 
   return <div className="pagination-dots">{dotsList}</div>;
